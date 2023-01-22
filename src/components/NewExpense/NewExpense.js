b257b2d37f33ad078574dbd7bce5291abf0c15ac
AndresUribe1234 <anduri1997@gmail.com>
2023-01-22 15:09:19 -0500

    form component ready with click and submit event

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,13 @@
+import "./NewExpense.scss";
+
+import NewExpenseForm from "./NewExpenseForm";
+
+const NewExpense = () => {
+  return (
+    <div className="new-expense">
+      <NewExpenseForm />
+    </div>
+  );
+};
+
+export default NewExpense;
